b045926ab502fa321407af57a3f91cc0b4241e2c
shivaprasad <shivaprasadnomula75@gmail.com>
2023-11-10 12:55:32 +0000

    solution

--- app.js
+++ app.js
@@ -1,4 +1,360 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+
+let db;
+
+const initializeDB = async () => {
+  db = await open({
+    filename: "twitterClone.db",
+    driver: sqlite3.Database,
+  });
+  await db.migrate({ force: "last" });
+};
+
+initializeDB();
+
+const SECRET_KEY = "your-secret-key";-
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Invalid JWT Token" });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid JWT Token" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+app.post("/register", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const selectUserQuery = "SELECT * FROM user WHERE username = ?";
+  const dbUser = await db.get(selectUserQuery, [username]);
+
+  if (dbUser === undefined) {
+    if (password.length < 6) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createUserQuery = `
+        INSERT INTO user (name, username, password, gender)
+        VALUES(?, ?, ?, ?);
+      `;
+      await db.run(createUserQuery, [name, username, hashedPassword, gender]);
+
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = "SELECT * FROM user WHERE username = ?";
+  const dbUser = await db.get(selectUserQuery, [username]);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const token = jwt.sign({ user_id: dbUser.user_id }, SECRET_KEY);
+      response.send({ jwtToken: token });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.get("/user/tweets/feed", verifyToken, async (request, response) => {
+  try {
+    const { user_id } = request.payload;
+    const getTweetsFeedQuery = `
+      SELECT 
+        username,
+        tweet,
+        date_time AS dateTime
+      FROM 
+        follower
+      INNER JOIN tweet ON follower.following_user_id = tweet.user_id
+      INNER JOIN user ON user.user_id = follower.following_user_id
+      WHERE 
+        follower.follower_user_id = ${user_id}
+      ORDER BY
+        date_time DESC
+      LIMIT 4
+    `;
+    const tweetFeedArray = await db.all(getTweetsFeedQuery);
+    response.send(tweetFeedArray);
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/user/following", verifyToken, async (request, response) => {
+  try {
+    const { user_id } = request.payload;
+    const userFollowsQuery = `
+      SELECT 
+        name
+      FROM 
+        user
+      INNER JOIN follower ON user.user_id = follower.following_user_id
+      WHERE 
+        follower.follower_user_id = ${user_id}
+    `;
+    const userFollowsArray = await db.all(userFollowsQuery);
+    response.send(userFollowsArray);
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/user/followers", verifyToken, async (request, response) => {
+  try {
+    const { user_id } = request.payload;
+    const userFollowersQuery = `
+      SELECT 
+        name
+      FROM
+        user
+      INNER JOIN follower ON user.user_id = follower.follower_user_id
+      WHERE 
+        follower.following_user_id = ${user_id}
+    `;
+    const userFollowersArray = await db.all(userFollowersQuery);
+    response.send(userFollowersArray);
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/tweets/:tweetId", verifyToken, async (request, response) => {
+  try {
+    const { tweetId } = request.params;
+    const { payload } = request;
+    const { user_id } = payload;
+    const tweetsQuery = `SELECT * FROM tweet WHERE tweet_id = ${tweetId}`;
+    const tweetsResult = await db.get(tweetsQuery);
+
+    const userFollowersQuery = `
+      SELECT 
+        *
+      FROM  follower
+      INNER JOIN user ON user.user_id = follower.following_user_id 
+      WHERE 
+        follower.follower_user_id  = ${user_id}
+    `;
+
+    const userFollowers = await db.all(userFollowersQuery);
+
+    if (
+      userFollowers.some(
+        (item) => item.following_user_id === tweetsResult.user_id
+      ) &&
+      userFollowers[0].follower_user_id === user_id
+    ) {
+      const getTweetDetailsQuery = `
+        SELECT
+          tweet,
+          COUNT(DISTINCT(like.like_id)) AS likes,
+          COUNT(DISTINCT(reply.reply_id)) AS replies,
+          tweet.date_time AS dateTime
+        FROM 
+          tweet
+        INNER JOIN like ON tweet.tweet_id = like.tweet_id
+        INNER JOIN reply ON reply.tweet_id = tweet.tweet_id
+        WHERE 
+          tweet.tweet_id = ${tweetId} AND tweet.user_id=${userFollowers[0].user_id}
+      `;
+      const tweetDetails = await db.get(getTweetDetailsQuery);
+      response.send(tweetDetails);
+    } else {
+      response.status(401);
+      response.send("Invalid Request");
+    }
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/tweets/:tweetId/likes", verifyToken, async (request, response) => {
+  try {
+    const { tweetId } = request.params;
+    const { payload } = request;
+    const { user_id } = payload;
+    const getLikedUsersQuery = `
+      SELECT 
+        *
+      FROM 
+        follower
+      INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+      INNER JOIN like ON like.tweet_id = tweet.tweet_id
+      INNER JOIN user ON user.user_id = like.user_id
+      WHERE 
+        tweet.tweet_id = ${tweetId} AND follower.follower_user_id = ${user_id}
+    `;
+    const likedUsers = await db.all(getLikedUsersQuery);
+
+    if (likedUsers.length !== 0) {
+      let likes = [];
+      const getNamesArray = (likedUsers) => {
+        for (let item of likedUsers) {
+          likes.push(item.username);
+        }
+      };
+      getNamesArray(likedUsers);
+      response.send({ likes });
+    } else {
+      response.status(401);
+      response.send("Invalid Request");
+    }
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/tweets/:tweetId/replies", verifyToken, async (request, response) => {
+  try {
+    const { tweetId } = request.params;
+    const { payload } = request;
+    const { user_id } = payload;
+    const getRepliedUsersQuery = `
+      SELECT 
+        *
+      FROM 
+        follower
+      INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+      INNER JOIN reply ON reply.tweet_id = tweet.tweet_id
+      INNER JOIN user ON user.user_id = reply.user_id
+      WHERE 
+        tweet.tweet_id = ${tweetId} AND follower.follower_user_id = ${user_id}
+    `;
+    const repliedUsers = await db.all(getRepliedUsersQuery);
+
+    if (repliedUsers.length !== 0) {
+      let replies = [];
+      const getNamesArray = (repliedUsers) => {
+        for (let item of repliedUsers) {
+          let object = {
+            name: item.name,
+            reply: item.reply,
+          };
+          replies.push(object);
+        }
+      };
+      getNamesArray(repliedUsers);
+      response.send({ replies });
+    } else {
+      response.status(401);
+      response.send("Invalid Request");
+    }
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.get("/user/tweets", verifyToken, async (request, response) => {
+  try {
+    const { user_id } = request.payload;
+    const getUserTweetsQuery = `
+      SELECT 
+        tweet,
+        date_time AS dateTime
+      FROM 
+        tweet
+      WHERE 
+        user_id = ${user_id}
+      ORDER BY
+        date_time DESC
+    `;
+    const userTweets = await db.all(getUserTweetsQuery);
+    response.send(userTweets);
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.post("/user/tweets", verifyToken, async (request, response) => {
+  try {
+    const { user_id } = request.payload;
+    const { tweet } = request.body;
+    if (!tweet || tweet.length < 1 || tweet.length > 280) {
+      response.status(400);
+      response.send("Bad Request: Tweet content must be 1-280 characters.");
+      return;
+    }
+    const insertTweetQuery = `
+      INSERT INTO tweet (user_id, tweet, date_time)
+      VALUES (${user_id}, "${tweet}", DATETIME('now'))
+    `;
+    await db.run(insertTweetQuery);
+    response.send("Created a Tweet");
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+app.delete("/tweets/:tweetId", verifyToken, async (request, response) => {
+  try {
+    const { tweetId } = request.params;
+    const { payload } = request;
+    const { user_id } = payload;
+
+    const tweetDeleteQuery = `
+      DELETE FROM tweet
+      WHERE 
+        tweet_id = ${tweetId} AND user_id = ${user_id}
+    `;
+    const result = await db.run(tweetDeleteQuery);
+
+    if (!result) {
+      response.status(404);
+      response.send("Tweet not found");
+    } else if (result.changes === 0) {
+      response.status(401);
+      response.send("Unauthorized: You can only delete your own tweets");
+    } else {
+      response.send("Tweet Removed");
+    }
+  } catch (error) {
+    response.status(500);
+    response.send("Internal Server Error");
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
+
+/* const express = require("express");
 const app = express();
 
 const bcrypt = require("bcrypt");
